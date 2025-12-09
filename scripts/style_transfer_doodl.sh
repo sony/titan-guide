c5ddbf860a8d7ad32b13276c568e9235e3cdf856
Christian Simon <jangancariaku@gmail.com>
2025-12-09 21:15:46 +0900

    Update style_transfer_doodl.sh

--- scripts/style_transfer_doodl.sh
+++ scripts/style_transfer_doodl.sh
@@ -7,10 +7,10 @@ model_name_or_path='emilianJR/epiCRealism'
 # task=temporalindicator_guidance
 task=style_transfer
 guide_network='openai/clip-vit-base-patch16'
-target='/mnt/data2/chris/code/Training-Free-Guidance/data/wikiart/0.png'
+target='./data/wikiart/0.png'
 recur_steps=1
 rho_schedule="decrease"
-save_path="/mnt/data2/chris/outputs/video_gen_guide/style_transfer/doodl_2/"
+save_path="./outputs/video_gen_guide/style_transfer/doodl_2/"
 
 
 train_steps=1000
