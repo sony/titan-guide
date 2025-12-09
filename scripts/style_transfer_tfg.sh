0f1d069053cb4b79ca7ab32de8b5c2c163dd86e5
Christian Simon <jangancariaku@gmail.com>
2025-12-09 21:17:03 +0900

    Update style_transfer_tfg.sh

--- scripts/style_transfer_tfg.sh
+++ scripts/style_transfer_tfg.sh
@@ -6,10 +6,10 @@ model_name_or_path='emilianJR/epiCRealism'
 
 task=style_transfer
 guide_network='openai/clip-vit-base-patch16'
-target='/mnt/data2/chris/code/Training-Free-Guidance/data/wikiart/0.png' 
+target='./data/wikiart/0.png' 
 recur_steps=1
 rho_schedule="decrease"
-save_path="/mnt/data2/chris/outputs/video_gen_guide/style_transfer/tfg/"
+save_path="./outputs/video_gen_guide/style_transfer/tfg/"
  
 train_steps=1000
 inference_steps=15 #15
